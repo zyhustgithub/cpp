2cb1fa2e321c8e8d860955e7f9fbc302ba145203
Zeng Yong <zengyonghust@foxmail.com>
2020-04-13 00:03:53 +0800

    string插入删除

--- learn_string.cpp
+++ learn_string.cpp
@@ -6,7 +6,8 @@ using namespace std;
 void TestStringUsage()
 {
     // 初始化
-    std::string str1(10, 'A');
+    int num = 10;
+    std::string str1(num, 'A');
     cout << str1 << endl;
     std::string str2 = "ABCD";
     cout << str2 << endl;
@@ -14,6 +15,14 @@ void TestStringUsage()
     // 子串
     cout << str1.substr(5) << endl;
     cout << str2.substr(1, 2) << endl;
+
+    // 插入
+    string str3 = "qwer";
+    str3.insert(2, "A"); // 位置索引，插入字符串
+    cout << str3 << endl;
+    // 删除
+    str3.erase(2, 1); // 位置索引，移除个数
+    cout << str3 << endl;
 }
 
 int main(int argc, char *argv[])
