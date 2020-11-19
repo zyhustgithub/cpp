81d70f8222ddcae49692575e625cf5c3904dcdc5
Zeng Yong <zengyonghust@foxmail.com>
2020-11-19 23:14:00 +0800

    定制化智能指针

--- 智能指针/build.sh
+++ 智能指针/build.sh
@@ -3,7 +3,10 @@
 BUILD_PATH=$(cd $(dirname $0) && pwd)
 echo "****************************************************************"
 echo "PATH:$BUILD_PATH"
-g++ $BUILD_PATH/智能指针管理数组.cpp -std=gnu++14 -o $BUILD_PATH/test
-$BUILD_PATH/test
+g++ $BUILD_PATH/智能指针管理数组.cpp -std=gnu++14 -o $BUILD_PATH/test1
+$BUILD_PATH/test1
+
+g++ $BUILD_PATH/定制化智能指针.cpp -std=gnu++14 -o $BUILD_PATH/test2
+$BUILD_PATH/test2
 echo "****************************************************************"
-rm $BUILD_PATH/test
+rm $BUILD_PATH/test*
